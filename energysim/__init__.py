ff136b3f87c2a05efd20fa42d45d43aab8debeea
Philip Hierhager <philip.hierhager@googlemail.com>
2025-10-07 10:02:06 +0200

    add Factory to init

--- energysim/__init__.py
+++ energysim/__init__.py
@@ -1,6 +1,8 @@
 from energysim.rl.building_environment import BuildingEnvironment, EnvironmentConfig
+from energysim.rl.factory import EnvironmentFactory
 
 __all__ = [
  "BuildingEnvironment",
  "EnvironmentConfig",
+ "EnvironmentFactory",
 ]
